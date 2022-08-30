dafeac7ce9c0307f2f6a5478fb42946bac808499
devon-17 <itisdevondunham@gmail.com>
2022-08-30 13:25:43 -0400

    done: timer

--- ClickerJam/Assets/Scripts/GameManager.cs
+++ ClickerJam/Assets/Scripts/GameManager.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("TIMER")]
     public float timer = 15;
     public float timeStartAmount = 5;
+    public GameObject timerObj;
+    public Text timerText;
+
+    [Header("MONEY")]
     public int money;
+    public int moneyIncreaseAmount;
+    public Text moneyText;
 
     private bool moneyNeedsToIncrease;
     private bool isClicked;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -28,18 +30,36 @@ public class GameManager : MonoBehaviour
         money += amount;
     }
 
-    public void StartTimer(){
+    // event trigger component on obj
+    public void StartTimer()
+    {
         isClicked = true;
     }
 
-    public void TimerManager(){
-        //buggy
-        if(isClicked){
-            timer -= Time.deltaTime;
-            isClicked = false;
+    public void TimerManager()
+    {
+        // when obj is clicked
+        if(isClicked)
+        {
+            timer -= Time.deltaTime; // start timer
+
+            // activating timer obj and setting text
+            timerObj.SetActive(true);
+            timerText.text = "Time Left: " + timer.ToString("f2");
+
+            // money needs to increase so we can add money
+            moneyNeedsToIncrease = true; 
         }
-        if(timer <= 0){
+        if(timer <= 0) // when 0
+        {
+            // reset timer
             timer = timeStartAmount;
+
+            timerObj.SetActive(false); // setting timer text to false
+            isClicked = false; // obj needs to be clicked again
+            AddMoney(moneyIncreaseAmount); // adding money using our int
+            moneyText.text = "Money: " + money; // setting text
+            moneyNeedsToIncrease = false; // telling game we have our money for that click
         }
     }
 }
